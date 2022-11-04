23173c1f09641bdd7bd0f466c344f2ed5199fbf5
yuxuanjoe <114721273+yuxuanjoe@users.noreply.github.com>
2022-11-04 14:22:24 +0800

    Add files via upload

---
+++ lab05.js
@@ -0,0 +1,36 @@
+function Width200() {
+  document.getElementById("t").style.width = "200px";
+  }
+function Width500() {
+  document.getElementById("t").style.width = "500px";
+  }
+function Border1px(){
+  document.getElementById("t").style.borderSpacing="1px";
+}
+function Border10px(){
+  document.getElementById("t").style.borderSpacing="10px";
+}
+function Border20px(){
+  document.getElementById("t").style.borderSpacing="20px";
+}
+function Colorgreen() {
+  document.getElementById("color").style.backgroundColor="green";
+  document.getElementById("color1").style.backgroundColor="green";
+  }
+function Colorblue() {
+  document.getElementById("color").style.backgroundColor="blue";
+  document.getElementById("color1").style.backgroundColor="blue";
+}
+function Colororange() {
+  document.getElementById("color").style.backgroundColor="orange";
+  document.getElementById("color1").style.backgroundColor="orange";
+}
+function Colorred() {
+  document.getElementById("color").style.backgroundColor="red";
+  document.getElementById("color1").style.backgroundColor="red";
+}
+function reset() {
+  document.getElementById("t").style.borderSpacing="1px";
+  document.getElementById("color").style.backgroundColor="yellow";
+  document.getElementById("color1").style.backgroundColor="yellow";
+}
